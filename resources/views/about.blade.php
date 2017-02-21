a2ff65bd4f38d80e272809344045d8b0d259b894
Mārcis <akonens@inbox.lv>
2017-02-21 20:23:08 +0200

    Revert "photo_website"
    This reverts commit 870172af0707e411f3e418df8a99932e44f15d80.

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>About</h1>
+    <a href="http://todo.dev/about">Sakums</a>
+    <a href="http://todo.dev/about">Sakums</a>
+    <a href="http://todo.dev/">Beigas</a>
+
+  </body>
--- routes/web.php
+++ routes/web.php
@@ -11,70 +11,17 @@
 |
 */
 
-Route::get('/apply', function () {
-    return view('apply');
-});
-
-Route::get('/contacts', function () {
-
-    return view('contacts');
-
-});
-
-Route::get('/forum', function () {
-
-    return view('forum');
-
-});
-
-Route::get('/gallery', function () {
-
-    return view('gallery');
-
-});
-
-Route::get('/news', function () {
-
-
-$id = 2;
-
-$data['id'] = $id;
-$data['Vards'] = 'Nikita';
-$data['Uzvards'] = 'Lavruhins';
-$data['Telefons'] = '288232352';
-$ids = 3;
-
-$data2['id'] = $ids;
-$data2['Vards'] = 'Jariks';
-$data2['Uzvards'] = 'Lavruhins';
-$data2['Telefons'] = '288232252';
-
-dd([$data, $data2]);
-    return view('news');
-
-});
-
 Route::get('/', function () {
-
     return view('welcome');
-
 });
 
-Route::get ('profile/{id}', function($id) {
+Route::get('about', function () {
 
+    return view('about');
 
-  $data = [
-    'diena' => 'Otriena',
-    'id' => $id,
-    'piecinieks' => '5',
-    'name' => "peteris",
-    'lietotajs' => \App\User::find($id)
-  ];
+});
+Route::get('/about/about', function () {
 
-
-  return view ('profile', $data);
-
-
-
+    return view('aboutabout');
 
 });
